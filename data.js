09e6a497c89816fdb2110f1e4c59d41166adbe9d
GitamGymRats <fearnotmanbrave@gmail.com>
2024-12-27 19:01:18 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+window.data = [
+    {
+        userName : "nveluri2@gitam.in",
+        password: "bu22csen0300100",
+        image : "images/nitesh.jpg"
+    },
+    {
+        userName : "agongadi@gitam.in",
+        password: "bu22csen0100426",
+        image : "images/akhilesh.jpg"
+    },
+]
